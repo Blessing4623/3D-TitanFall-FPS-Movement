af588e9e9beab95f839920ca5a45f3c33e659725
Blessing Denis <fargoxyt@gmail.com>
2024-08-29 16:35:29 +0100

    Sorry wasnt paying attention

---
+++ tryingstuff.cs
@@ -0,0 +1,14 @@
+using system;
+using unity game engine;
+class justplaying : Monobehavior;
+start()
+{
+    // start is called after the first frame update
+}
+update ()
+{
+    // update is called after the first frame update 
+    /*were gonna play this imaginary game at 1000000000000 frames per second
+    dosent make sense huh thats why its called just playing dummy */
+    
+}
